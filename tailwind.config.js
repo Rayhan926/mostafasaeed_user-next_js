0e46b058a0c41b5b5511fc71067694e4780a53c1
rayhan926 <rayhanbd7075@gmail.com>
2022-02-11 11:10:25 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: "#65c8df",
+          100: "#2DA5D8",
+          200: "#438ab3",
+          300: "#3171a5",
+          400: "#285E98",
+          500: "#165187",
+          600: "#0E4975",
+          700: "#0F3B68",
+          800: "#05294E",
+          900: "#021E3A",
+        }
+      }
+    },
+  },
+  plugins: [],
+}
